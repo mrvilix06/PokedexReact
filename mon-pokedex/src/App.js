bb418dbc59cbbc1576c81436e0bdcee8dccfcb5c
mrvilix06 <smaurin18@hotmail.fr>
2023-09-12 14:37:52 +0200

    wiop

--- mon-pokedex/src/App.js
+++ mon-pokedex/src/App.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useRef } from 'react';
+import React, { useState, useEffect, useRef, useCallback } from 'react';
 import './App.css';
 
 const types = {
@@ -21,106 +21,85 @@ const types = {
 };
 
 function App() {
-  const [allPokemon, setAllPokemon] = useState([]);
   const [displayedPokemon, setDisplayedPokemon] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState(null);
   const [searchTerm, setSearchTerm] = useState("");
-  const [index, setIndex] = useState(21);
-  const chargementRef = useRef(null);
+  const [index, setIndex] = useState(0);
 
 
-  useEffect(() => {
+useEffect(() => {
     fetchPokemonBase();
   }, []);
+
+  const handleScroll = () => {
+    const { scrollTop, scrollHeight, clientHeight } = document.documentElement;
+    if (clientHeight + scrollTop >= scrollHeight - 20 && !isLoading) {
+      fetchPokemonBase();
+    }
+  };
 
   useEffect(() => {
     window.addEventListener('scroll', handleScroll);
     return () => window.removeEventListener('scroll', handleScroll);
-  }, [index]);
+  }, [isLoading]);
 
-  const handleScroll = () => {
-    const { scrollTop, scrollHeight, clientHeight } = document.documentElement;
-    if (clientHeight + scrollTop >= scrollHeight - 20) {
-      addPoke(6);
+  const fetchPokemonBase = async () => {
+    setIsLoading(true);
+    setError(null);
+    try {
+      if(index > 151) return;
+      const response = await fetch(`https://pokeapi.co/api/v2/pokemon?offset=${index}&limit=20`);
+      const data = await response.json();
+      const newPokemon = await Promise.all(data.results.map(fetchPokemonComplet));
+      setDisplayedPokemon(prev => [...prev, ...newPokemon]);
+      setIndex(prevIndex => prevIndex + 20);
+    } catch (error) {
+      setError(error);
+    } finally {
+      setIsLoading(false);
     }
   };
 
-  const addPoke = (nb) => {
-    if (index > 151) return;
-    const arrToAdd = allPokemon.slice(index, index + nb);
-    setDisplayedPokemon([...displayedPokemon, ...arrToAdd]);
-    setIndex(index + nb);
+  const fetchPokemonComplet = async (pokemon) => {
+    const url = pokemon.url;
+    const response = await fetch(url);
+    const pokeData = await response.json();
+    return {
+      pic: pokeData.sprites.front_default,
+      type: pokeData.types[0].type.name,
+      id: pokeData.id,
+      name: pokeData.name
+    };
   };
 
-
-  const fetchPokemonBase = () => {
-    fetch("https://pokeapi.co/api/v2/pokemon?limit=151")
-    .then(reponse => reponse.json())
-    .then((allPoke) => {
-        allPoke.results.forEach((pokemon) => {
-            fetchPokemonComplet(pokemon);
-        })
-    })
-
-  };
-
-  useEffect(() => {
-    if (allPokemon.length === 151) {
-      const tableauFin = allPokemon.sort((a, b) => a.id - b.id).slice(0, 21);
-      setDisplayedPokemon(tableauFin);
-      if (chargementRef.current) {
-        chargementRef.current.style.display = "none";
-      }
-    }
-  }, [allPokemon]);
-
-  const fetchPokemonComplet = (pokemon) => {
-    let objPokemonFull = {};
-    let url = pokemon.url;
-    let nameP = pokemon.name;
-
-    fetch(url)
-        .then(reponse => reponse.json())
-        .then((pokeData) => {
-            objPokemonFull.pic = pokeData.sprites.front_default;
-            objPokemonFull.type = pokeData.types[0].type.name;
-            objPokemonFull.id = pokeData.id;
-
-            fetch(`https://pokeapi.co/api/v2/pokemon-species/${nameP}`)
-                .then(reponse => reponse.json())
-                .then((pokeData) => {
-                    objPokemonFull.name = pokeData.names[4].name;
-                    setAllPokemon(currentList => [...currentList, objPokemonFull]);
-                })
-        })
-  };
-
-  const handleSearch = (e) => {
+  const handleSearch = useCallback((e) => {
     setSearchTerm(e.target.value);
-    // ... (même code que dans votre code JavaScript pour la recherche)
-  };
+  }, []);
 
   return (
     <div className="App">
-      <h1>Pokedex</h1>
-      <form className="recherche-poke">
-        <label htmlFor="recherche">Rechercher un pokémon</label>
-        <input type="text" id="recherche" value={searchTerm} onChange={handleSearch} />
-      </form>
-      <div className="loader" ref={chargementRef}>
-        <img src="pokeball.png" alt="pokeball" />
-      </div>
-      <div className="container">
+    <h1>Pokedex</h1>
+    <form className="recherche-poke">
+      <label htmlFor="recherche">Rechercher un pokémon</label>
+      <input type="text" id="recherche" value={searchTerm} onChange={handleSearch} />
+    </form>
+    <div className="container">
       <ul className="liste-poke">
         {displayedPokemon.map((poke, index) => (
-          <li key={index} style={{ background: types[poke.type] }}>
+          <li key={poke.id} style={{ background: types[poke.type] }}>
             <img src={poke.pic} alt={poke.name} />
             <h5>{poke.name}</h5>
             <p>ID# {poke.id}</p>
           </li>
         ))}
       </ul>
-      </div>
     </div>
+    {isLoading && <div className="loader">
+      <img src="pokeball.png" alt="pokeball" />
+    </div>}
+    {error && <p>Error: {error.message}</p>}
+  </div>
   );
 }
 
